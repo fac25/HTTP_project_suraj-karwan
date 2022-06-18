71287d77aa70b4901067bb6552ead7f2bd35f1f4
karwanm <karwan.ismo@gmail.com>
2022-06-19 00:58:58 +0100

    fix slide-show, box-office layout and get random giphies

--- script.js
+++ script.js
@@ -14,3 +14,27 @@ function getMovies(url) {
       console.log(data);
     });
 }
+
+
+
+// Giphy API
+
+const giphyUrl = document.getElementsByClassName("giphy")
+const giphyTitle = document.getElementsByClassName("giphy-title")
+
+getGiphy = () => {
+  fetch(
+    "https://api.giphy.com/v1/gifs/trending?api_key=sGSQS2XOW32LeNaUXeW9c9UeWP4MiWtI&limit=25&rating=g"
+  )
+    .then((res) => res.json())
+    .then((giphies) => {
+
+      for(i = 0; i < giphies.data.length; i++){
+        giphyUrl[i].src = `${giphies.data[i].images.original.url}`
+        giphyTitle[i].innerHTML = `${giphies.data[i].title}`
+      }
+
+    });
+};
+
+getGiphy();
