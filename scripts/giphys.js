b55cc6977a3f2ccf90334f47c137202c1ad6583a
karwanm <karwan.ismo@gmail.com>
2022-06-21 01:04:42 +0100

    fix hover over posters, slide show and Giphy search

--- scripts/giphys.js
+++ scripts/giphys.js
@@ -1,31 +1,51 @@
 // Giphy API
 
+// Giphy page layout
+
+giphyLayout = (giphyData) =>{
+  for (i = 0; i < giphyData.data.length; i++) {
+    const giphyContainer = document.createElement("div");
+    const giphy = document.createElement("img");
+    const giphyTitle = document.createElement("p");
+
+    giphyContainer.classList.add("giphy-container");
+    giphy.classList.add("giphy");
+    giphyTitle.classList.add("giphy-title");
+
+    giphy.src = `${giphyData.data[i].images.original.url}`;
+    giphyTitle.innerHTML = `${giphyData.data[i].title}`;
+
+    giphyContainer.append(giphy);
+    giphyContainer.append(giphyTitle);
+
+    giphyMainContainer.append(giphyContainer);
+  }
+}
+
 const giphyMainContainer = document.getElementById("giphy-main-container");
 
-getGiphy = async () => {
+// Get Random Giphys
+
+fetch(
+  "https://api.giphy.com/v1/gifs/trending?api_key=sGSQS2XOW32LeNaUXeW9c9UeWP4MiWtI&limit=25&rating=g"
+)
+  .then((res) => res.json())
+  .then((giphies) => {
+    giphyLayout(giphies)
+  });
+
+// Search for Giphys
+
+const giphySearch = document.getElementById("giphy-search");
+const giphyInputSearch = document.getElementById("giphy-input-search");
+
+giphySearch.addEventListener("click", async() => {
   await fetch(
-    "https://api.giphy.com/v1/gifs/trending?api_key=sGSQS2XOW32LeNaUXeW9c9UeWP4MiWtI&limit=25&rating=g"
-  )
-    .then((res) => res.json())
-    .then((giphies) => {
-      for (i = 0; i < giphies.data.length; i++) {
-        const giphyContainer = document.createElement("div");
-        const giphy = document.createElement("img");
-        const giphyTitle = document.createElement("p");
+    `https://api.giphy.com/v1/gifs/search?api_key=sGSQS2XOW32LeNaUXeW9c9UeWP4MiWtI&q=${giphyInputSearch.value}&limit=25&offset=0&rating=g&lang=en`
+  ).then((res) => res.json())
+  .then(giphyData => {
+    giphyLayout(giphyData)
+  })
 
-        giphyContainer.classList.add("giphy-container");
-        giphy.classList.add("giphy");
-        giphyTitle.classList.add("giphy-title");
-
-        giphy.src = `${giphies.data[i].images.original.url}`;
-        giphyTitle.innerHTML = `${giphies.data[i].title}`;
-
-        giphyContainer.append(giphy);
-        giphyContainer.append(giphyTitle);
-
-        giphyMainContainer.appendChild(giphyContainer);
-      }
-    });
-};
-
-getGiphy();
+  giphyInputSearch.value = "";
+});
