c8a45e68eb93f4c340b8504a00650d9a4f0c7561
Suraj Pun <s_pun@outlook.com>
2022-06-21 17:32:37 +0100

    updated movie page

--- scripts/movies.js
+++ scripts/movies.js
@@ -1,7 +1,99 @@
 // Movie API Box Office
 
 // Movie API
+movieLayout = (movieData) => {
+  
+  for (i = 0; i < movieData.results.length; i++) {
+      const {title, poster_path, vote_average, overview, original_language, release_date, popularity} = movieData;  
 
+      const movieContainer = document.createElement("div");
+      movieContainer.classList.add('movie-wrapper')
+      movieContainer.innerHTML = `
+            <a href="../html/movie-info.html" target="_blank" class="movie">
+
+              <img class="movie-poster" src="https://image.tmdb.org/t/p/w500${movieData.results[i].poster_path}" alt="${movieData.results[i].title}"/>
+              <div class="shade"></div>
+              <span class="movie-popularity"><strong>R: </strong>${Math.trunc(movieData.results[i].popularity)}</span>
+              <span class="movie-duration">125mins</span>
+              <div class="movie-detail-wrapper">
+                <span class="movie-language">${movieData.results[i].original_language.toUpperCase()}</span>
+                <span class="movie-rate">${movieData.results[i].vote_average}</span>
+                <span class="movie-age">12A</span>
+              </div>
+
+              <p class="movie-name">
+              ${movieData.results[i].title}
+              <br>
+              <span class="movie-release-year"><em>${movieData.results[i].release_date}</em></span>
+              </p>
+
+            </a>
+      `
+
+      movieMainContainer.appendChild(movieContainer);
+  }
+};
+
+const movieMainContainer = document.getElementById("movies");
+
+// Movie fetch
+
+fetch(
+  "https://api.themoviedb.org/3/discover/movie?api_key=6ee6d2c638a53edad93b9bf9fe334f43&page=1"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    movieLayout(data);
+  });
+
+// Slide show
+
+// Slide show elements
+
+const slideShow = document.getElementById("slides");
+
+fetch(
+  "https://api.themoviedb.org/3/discover/movie?api_key=6ee6d2c638a53edad93b9bf9fe334f43&page=1"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    // Slide show
+    for (i = 0; i < 5; i++) {
+      const slide = document.createElement("img");
+      const slideWrapper = document.createElement("div");
+
+      slide.src = `https://image.tmdb.org/t/p/w500${data.results[i].backdrop_path}`;
+      slide.alt = data.results[i].title + " poster";
+      slideWrapper.classList.add("slide");
+
+      if (i === 0) {
+        slideWrapper.classList.add("slide-1");
+      }
+      slideWrapper.append(slide);
+      slideShow.append(slideWrapper);
+    }
+  });
+
+// Search for Movies
+
+const movieSearch = document.getElementById("movie-search");
+const movieInputSearch = document.getElementById("movie-input-search");
+
+movieSearch.addEventListener("click", async () => {
+  await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=6ee6d2c638a53edad93b9bf9fe334f43&query=${movieInputSearch.value}`
+  )
+    .then((res) => res.json())
+    .then((movieData) => {
+      movieLayout(movieData);
+    });
+
+  movieInputSearch.value = "";
+});
+
+// HTML Movie JS
+/* 
 // Movies elements
 
 const moviePoster = document.getElementsByClassName("movie-poster");
@@ -63,31 +155,5 @@ getMovie = async () => {
 };
 
 getMovie();
+*/
 
-// Slide show
-
-// Slide show elements
-
-const slideShow = document.getElementById("slides");
-
-fetch(
-  "https://api.themoviedb.org/3/discover/movie?api_key=6ee6d2c638a53edad93b9bf9fe334f43&page=1"
-)
-  .then((res) => res.json())
-  .then((data) => {
-    // Slide show
-    for (i = 0; i < 5; i++) {
-      const slide = document.createElement("img");
-      const slideWrapper = document.createElement("div");
-
-      slide.src = `https://image.tmdb.org/t/p/w500${data.results[i].backdrop_path}`;
-      slide.alt = data.results[i].title + " poster";
-      slideWrapper.classList.add("slide");
-
-      if (i === 0) {
-        slideWrapper.classList.add("slide-1");
-      }
-      slideWrapper.append(slide);
-      slideShow.append(slideWrapper);
-    }
-  });
