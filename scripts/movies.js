e4ddd4bcad6a41dfaa951e6996669932b4482309
Suraj Pun <s_pun@outlook.com>
2022-06-22 17:04:04 +0100

    fixed bugs

--- scripts/movies.js
+++ scripts/movies.js
@@ -46,46 +46,11 @@ fetch(
   .then((data) => {
     console.log(data);
     movieLayout(data);
-  });
+  })
+  .catch((error) => {
+    console.log(error)
+  });;;
 
-// Movies Pageination
-
-const next = document.querySelector('.next-page');
-let num = 1;
-const prev = document.querySelector('.prev-page');
-
-// Next page
-
-next.addEventListener('click', () => {
-        num++;
-        changePage();
-    })
-
-function changePage() {
-  movieMainContainer.innerHTML = "";
-
-  if(true) {
-    fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=6ee6d2c638a53edad93b9bf9fe334f43&page=${num}`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        movieLayout(data);
-      });
-  }
-}
-
-// Previous page
-
-prev.addEventListener('click', () => {
-  if(num > 1) {
-  num--;
-  changePage();
-  } else {
-    return;
-  }
-})
 
 // Slide show elements
 
@@ -111,7 +76,10 @@ fetch(
       slideWrapper.append(slide);
       slideShow.append(slideWrapper);
     }
-  });
+  })
+  .catch((error) => {
+    console.log(error)
+  });;;
 
 // Search for Movies
 
@@ -126,7 +94,10 @@ movieSearch.addEventListener("click", async () => {
     .then((res) => res.json())
     .then((movieData) => {
       movieLayout(movieData);
-    });
+    })
+    .catch((error) => {
+      console.log(error)
+    });;;
 
   movieInputSearch.value = "";
 });
