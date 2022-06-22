9f21514db81ecc153056047c386bb6b20a72a260
Suraj Pun <s_pun@outlook.com>
2022-06-22 16:36:52 +0100

    added paginaton

--- scripts/movies.js
+++ scripts/movies.js
@@ -48,14 +48,44 @@ fetch(
     movieLayout(data);
   });
 
-// Movies Pages Function
+// Movies Pageination
 
+const next = document.querySelector('.next-page');
+let num = 1;
+const prev = document.querySelector('.prev-page');
 
+// Next page
 
+next.addEventListener('click', () => {
+        num++;
+        changePage();
+    })
 
+function changePage() {
+  movieMainContainer.innerHTML = "";
 
+  if(true) {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=6ee6d2c638a53edad93b9bf9fe334f43&page=${num}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        movieLayout(data);
+      });
+  }
+}
 
-// Slide show
+// Previous page
+
+prev.addEventListener('click', () => {
+  if(num > 1) {
+  num--;
+  changePage();
+  } else {
+    return;
+  }
+})
 
 // Slide show elements
 
